e7bea0fbf50fec9ab8d03825432bf2404c1b164c
User <user@example.com>
2025-07-04 21:29:07 -0700

    Use partition labels (EFI/ROOT) for portable disk configuration

--- templates/starter/modules/nixos/disk-config.nix
+++ templates/starter/modules/nixos/disk-config.nix
@@ -8,7 +8,7 @@ _: {
   # Other examples found here: https://github.com/nix-community/disko/tree/master/example
   disko.devices = {
     disk = {
-      vda = {
+      main = {
         device = "/dev/%DISK%";
         type = "disk";
         content = {
@@ -17,23 +17,38 @@ _: {
             ESP = {
               type = "EF00";
               size = "1G";
+              label = "EFI";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "defaults" ];
               };
             };
             root = {
               size = "100%";
+              label = "ROOT";
               content = {
                 type = "filesystem";
                 format = "xfs";
                 mountpoint = "/";
+                mountOptions = [ "defaults" ];
               };
             };
           };
         };
       };
     };
+  };
+  
+  # Use partition labels for mounting instead of device paths
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/EFI";
+    fsType = "vfat";
+  };
+  
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/ROOT";
+    fsType = "xfs";
   };
 }
