aee2d7a2f92cd5ee2b886dafc1192ac9169ca389
User <user@example.com>
2025-07-06 04:47:14 -0700

    Fix ISO flake for unstable channel

--- iso/flake.nix
+++ iso/flake.nix
@@ -2,7 +2,7 @@
   description = "Custom NixOS installation ISO with SSH access";
   
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   
   outputs = { self, nixpkgs }: {
